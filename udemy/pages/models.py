7fcacbdaaf9c8458d6f6082ad9ca19bd7b680121
derick-droid <derrickokinda9@gmail.com>
2022-03-12 14:45:41 +0300

    creating superuser

--- udemy/pages/models.py
+++ udemy/pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=2555)
+    designation = models.CharField(max_length=255)
+    photos = models.ImageField(upload_to = 'photos/%y/%m/%d/')
+    facebook_link  = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    goodle_plus_link = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.first_name
