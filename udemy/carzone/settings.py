ac7478cdb07cdf6ee1e5d214fb4d782aa985c909
derick-droid <derrickokinda9@gmail.com>
2022-03-12 13:53:25 +0300

    postgresql configuration

--- udemy/carzone/settings.py
+++ udemy/carzone/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'carzone_db',
+        'USER':'postgres',
+        'PASSWORD': 'derrickokinda9@go',
+        'HOST':'localhost',
     }
 }
 
