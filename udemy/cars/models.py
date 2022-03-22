d491b39041e7139bd36e9cb94e83486005aaffea
derick-droid <derrickokinda9@gmail.com>
2022-03-22 15:27:18 +0300

    creating car model and making migrations

--- udemy/cars/models.py
+++ udemy/cars/models.py
@@ -1,3 +1,114 @@
+from distutils.command.upload import upload
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Cars(models.Model):
+    state_choice = (
+         ('AL', 'Alabama'),
+        ('AK', 'Alaska'),
+        ('AZ', 'Arizona'),
+        ('AR', 'Arkansas'),
+        ('CA', 'California'),
+        ('CO', 'Colorado'),
+        ('CT', 'Connecticut'),
+        ('DE', 'Delaware'),
+        ('DC', 'District Of Columbia'),
+        ('FL', 'Florida'),
+        ('GA', 'Georgia'),
+        ('HI', 'Hawaii'),
+        ('ID', 'Idaho'),
+        ('IL', 'Illinois'),
+        ('IN', 'Indiana'),
+        ('IA', 'Iowa'),
+        ('KS', 'Kansas'),
+        ('KY', 'Kentucky'),
+        ('LA', 'Louisiana'),
+        ('ME', 'Maine'),
+        ('MD', 'Maryland'),
+        ('MA', 'Massachusetts'),
+        ('MI', 'Michigan'),
+        ('MN', 'Minnesota'),
+        ('MS', 'Mississippi'),
+        ('MO', 'Missouri'),
+        ('MT', 'Montana'),
+        ('NE', 'Nebraska'),
+        ('NV', 'Nevada'),
+        ('NH', 'New Hampshire'),
+        ('NJ', 'New Jersey'),
+        ('NM', 'New Mexico'),
+        ('NY', 'New York'),
+        ('NC', 'North Carolina'),
+        ('ND', 'North Dakota'),
+        ('OH', 'Ohio'),
+        ('OK', 'Oklahoma'),
+        ('OR', 'Oregon'),
+        ('PA', 'Pennsylvania'),
+        ('RI', 'Rhode Island'),
+        ('SC', 'South Carolina'),
+        ('SD', 'South Dakota'),
+        ('TN', 'Tennessee'),
+        ('TX', 'Texas'),
+        ('UT', 'Utah'),
+        ('VT', 'Vermont'),
+        ('VA', 'Virginia'),
+        ('WA', 'Washington'),
+        ('WV', 'West Virginia'),
+        ('WI', 'Wisconsin'),
+        ('WY', 'Wyoming'),
+    )
+    year_choice = []
+    for year in range(2000,(datetime.now().year + 1)):
+        year_choice.append((year, year))
+        
+    features_choices = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+    
+    door_choices = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+    
+    car_title = models.CharField(max_length=250)
+    state = models.CharField(choices=state_choice, max_length=250)
+    colour = models.CharField(max_length=250)
+    model = models.CharField(max_length=250)
+    year = models.IntegerField(choices=year_choice)
+    price = models.IntegerField()
+    description = models.TextField(max_length=250)
+    car_photo = models.ImageField(upload_to = 'photos/%y/%m/%d/',    blank = True)
+    car_photo_1 = models.ImageField(upload_to = 'photos/%y/%m/%d/',  blank = True)
+    car_photo_2 = models.ImageField(upload_to = 'photos/%y/%m/%d/',  blank = True)
+    car_photo_3 = models.ImageField(upload_to = 'photos/%y/%m/%d/',  blank = True)
+    car_photo_4 =models.ImageField(upload_to = 'photos/%y/%m/%d/',   blank = True)
+    features = models.CharField(choices= features_choices, max_length=100)
+    body_style = models.CharField(max_length=250)
+    engine = models.CharField(max_length=250)
+    transmission = models.CharField(max_length=250)
+    interior = models.CharField(max_length=250)
+    miles = models.IntegerField()
+    doors = models.CharField(choices= door_choices, max_length=100)
+    passengers = models.IntegerField()
+    vin_no = models.CharField(max_length=250)
+    milage = models.IntegerField()
+    fuel_type = models.CharField(max_length=250)
+    no_of_owners = models.CharField(max_length=250)
+    is_featured = models.BooleanField(max_length=250)
+    created_date = models.DateTimeField(default=datetime.now,blank=True)
+    
