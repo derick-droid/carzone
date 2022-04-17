035145ec94d0cc804b26eaaecdd62d09cc1ebe73
derick-droid <derrickokinda9@gmail.com>
2022-04-17 08:41:28 +0300

    featured cars

--- udemy/pages/views.py
+++ udemy/pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Team
+from cars.models import Cars
+
 
 def home(request):
     teams  = Team.objects.all()
+    featured_cars = Cars.object.order_by("created_date")
+    
     data = {
         'teams' : teams,
+        'featured_cars':featured_cars,
     }
     
     return render (request,'pages/home.html', data)
